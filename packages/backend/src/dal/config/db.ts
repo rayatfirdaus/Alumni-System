2788f8bb825c92e5c3093cd3046a33d1e33e9d3c
Rayat <rayatsarkar@gmail.com>
2026-08-13 20:02:08 +0600

    added dal

---
+++ packages/backend/src/dal/config/db.ts
@@ -0,0 +1,27 @@
+import pkg from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const {Pool} = pkg;
+
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+});
+
+async function verifyCOnnection() {
+    try {
+        const client = await pool.connect();
+        console.log('Connected to PostgreSQL database');
+        client.release();
+    }
+    catch (error) {
+        console.error('Error connecting to PostgreSQL database', error);
+    }
+}
+
+verifyCOnnection();
+export default pool;
